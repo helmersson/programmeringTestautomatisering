19deaa9acb7dbb45eec30e69664acaa3298f79d3
Helmersson <50464822+helmersson@users.noreply.github.com>
2025-01-22 16:53:46 +0100

    La till en try på scanner för att spara på resurser och ta bort en kod rad

--- src/assignmentOne/MorseMain.java
+++ src/assignmentOne/MorseMain.java
@@ -5,33 +5,32 @@ import  java.util.Scanner;
 public class MorseMain {
     public static void main(String[] args) {
         MorseLogic logic = new MorseLogic();
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        while(true) {
-            //  Meny för användaren
-            System.out.println("Välj konverteringstyp");
-            System.out.println("1. Morsekod till engelska");
-            System.out.println("2. Engelska till morsekod");
-            System.out.println("3. Avsluta");
+            while (true) {
+                //  Meny för användaren
+                System.out.println("Välj konverteringstyp");
+                System.out.println("1. Morsekod till engelska");
+                System.out.println("2. Engelska till morsekod");
+                System.out.println("3. Avsluta");
 
-            //  Läser användarens input
-            int menyVal = scanner.nextInt();
-            scanner.nextLine();
+                //  Läser användarens input
+                int menyVal = scanner.nextInt();
+                scanner.nextLine();
 
-            //  Om val 3 avsluta loopen
-            if (menyVal == 3) break;
+                //  Om val 3 avsluta loopen
+                if (menyVal == 3) break;
 
-            System.out.println("Ange text att konvertera");
-            String input = scanner.nextLine();
+                System.out.println("Ange text att konvertera");
+                String input = scanner.nextLine();
 
-            try {
-                String result = (menyVal == 1) ? logic.morseToEnglish(input) : logic.englishToMorse(input);
-                System.out.println("Resultat: " + result);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Fel: " + e.getMessage());
+                try {
+                    String result = (menyVal == 1) ? logic.morseToEnglish(input) : logic.englishToMorse(input);
+                    System.out.println("Resultat: " + result);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Fel: " + e.getMessage());
+                }
             }
         }
-        //  Stänger scannern när programmet avslutats för att spara resurser
-        scanner.close();
     }
 }
